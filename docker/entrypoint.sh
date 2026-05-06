ed27b074d0deef3866ab52a92355baa3bae743b4
Edwarmo <edwar2020morales@gmail.com>
2026-05-06 09:12:35 -0500

    deploy v2.4

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 set -e
-
 cd /var/www
-
-# Limpiar cache previo del build
 rm -f bootstrap/cache/config.php
-rm -f bootstrap/cache/routes*.php
-
-# Cachear usando vars de entorno de Render directamente
+rm -f bootstrap/cache/routes-v7.php
 php artisan config:cache
 php artisan route:cache
-
-# Migraciones
 php artisan migrate --force
-
-# Servicios
 php-fpm -D
 nginx -g 'daemon off;'
