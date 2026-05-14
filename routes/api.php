79a38ae1eec67ad73fff4449ae4524a1895d26e1
Edwarmo <edwar2020morales@gmail.com>
2026-05-14 15:36:44 -0500

    fix: tema oscuro en html y ruta name para api.slots

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\SpaceSlotsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/spaces/{space:slug}/slots', SpaceSlotsController::class);
+Route::get('/spaces/{space:slug}/slots', SpaceSlotsController::class)->name('api.slots');
